67ea3c4490418f4c75bdfe87d9b2d073a0b0c341
AlexGsilversat <alexg@silversat.org>
2024-01-04 15:08:33 -0500

    init commit

--- TEST/ROBOFRC-TEST/src/main/java/frc/robot/ArmRoller.java
+++ TEST/ROBOFRC-TEST/src/main/java/frc/robot/ArmRoller.java
@@ -1,4 +1,4 @@
-package frc.robot.Arm;
+package frc.robot;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
@@ -6,8 +6,6 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.team5115.Classes.Accessory.Angle;
-import frc.team5115.Classes.Accessory.I2CHandler;
 
 public class ArmRoller {
     //CANSparkMax Motor1 = new CANSparkMax()
