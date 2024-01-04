d9750e4139631a1c0148cba77d5f90ad3da14569
vincent <vinceajohnson8@gmail.com>
2024-01-04 16:03:25 -0500

    Ayden a bum as bum bum

--- ArmAngle.java
+++ ArmAngle.java
@@ -1,5 +1,69 @@
 package frc.robot.Arm;
+import com.revrobotics.CANSparkMax;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.team5115.Classes.Accessory.Angle;
+import frc.team5115.Classes.Hardware.HardwareArm;
+import edu.wpi.first.math.controller.PIDController;
 
-public class ArmAngle {
+public class Armangle extends SubsystemBase {
+    public static final double Minimum_Degrees = 0.0;
+    public static final double Maximum_Degrees = 90.0;
+    private static final double TURN_PID_TOLERANCE = 0.0;
+    private static final double TURN_PID_KP = 0.04;
+    private static final double TURN_PID_KI = 0.0;
+    private static final double TURN_PID_KD = 0.0004;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
 }
