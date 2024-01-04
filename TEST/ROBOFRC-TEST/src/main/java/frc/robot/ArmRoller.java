1524f5eb353c718dc460d83815259d2e1b0833be
Scorchify <139656419+Scorchify@users.noreply.github.com>
2024-01-04 15:16:37 -0500

    Vincent is a bum

---
+++ TEST/ROBOFRC-TEST/src/main/java/frc/robot/ArmRoller.java
@@ -0,0 +1,12 @@
+package frc.robot;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ArmRoller {
+    //CANSparkMax Motor1 = new CANSparkMax()
+}
