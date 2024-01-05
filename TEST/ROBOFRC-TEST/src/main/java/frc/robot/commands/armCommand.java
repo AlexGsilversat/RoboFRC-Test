0ecac5314208358525fca05ac775e74678463446
Scorchify <139656419+Scorchify@users.noreply.github.com>
2024-01-05 15:56:33 -0500

    good

--- TEST/ROBOFRC-TEST/src/main/java/frc/robot/commands/armCommand.java
+++ TEST/ROBOFRC-TEST/src/main/java/frc/robot/commands/armCommand.java
@@ -1,5 +1,25 @@
 package frc.robot.commands;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class armCommand {
+
+public class armCommand extends CommandBase {
+    public spinMotorSlow() {
+        Tread tread = new Tread();
+        tread.motor1ks = 1; //whatever value to make it "slow"
+        tread.motor1v = 1; 
+        SoftwareArmRoller armRoller = new SoftwareArmRoller();
+        armRoller.intakeBall();
+    }
     
+    @Override
+    public void initialize() {
+        spinMotorSlow();
+    }
+
+ 
+
+
+
 }
+    
+
